c921211b06e5c03445268a90b8fd0ae2d8ccb6c2
jonathanmics <123796342+jonathanmics@users.noreply.github.com>
2024-10-09 05:11:02 -0400

    Update auth.php

--- auth.php
+++ auth.php
@@ -4,6 +4,7 @@ $auth_data = $_GET;
 define('TELEGRAM_BOT_TOKEN', '8083830932:AAGghWJmJYIlnV0A_huu0x8HckQMT9B_Wcs');
 
 // Function to check the auth data
+
 function checkTelegramAuthorization($auth_data) {
     $check_hash = $auth_data['hash'];
     unset($auth_data['hash']);
